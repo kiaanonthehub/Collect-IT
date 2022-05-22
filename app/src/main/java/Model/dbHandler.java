3067e5f2a35e14310726b70556d14941ad6fc70b
Kiaan Maharaj <kiaan.maahraj@outlook.com>
2022-05-22 19:05:32 +0200

    navigation drawer integrated - tested , passed

--- app/src/main/java/Model/dbHandler.java
+++ app/src/main/java/Model/dbHandler.java
@@ -1,5 +1,4 @@
 package Model;
 
-public class dbHandler {
-
+public class DbHandler {
 }
---
+++ app/src/main/java/com/kiaan/collect_it/ui/collection/CollectionFragment.java
@@ -0,0 +1,28 @@
+package com.kiaan.collect_it.ui.collection;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.kiaan.collect_it.R;
+
+public class CollectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_collection, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
