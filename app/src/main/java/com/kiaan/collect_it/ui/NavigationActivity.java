0ba22e61ea22e3204bf765faaf1b74f4c30baad4
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-05-30 09:22:13 +0200

    removed view items navigation and fragment

--- app/src/main/java/com/kiaan/collect_it/ui/NavigationActivity.java
+++ app/src/main/java/com/kiaan/collect_it/ui/NavigationActivity.java
@@ -41,7 +41,7 @@ public class NavigationActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_collection, R.id.nav_item,
+                R.id.nav_home, R.id.nav_item,
                 R.id.nav_logout, R.id.nav_create_collection,
                 R.id.nav_create_item, R.id.nav_about_us,
                 R.id.nav_contact_us)
