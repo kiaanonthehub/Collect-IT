7ba7fb243e40b382924d69b6e4f697f423a8ac07
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-06-26 23:09:09 +0200

    progress bar added, needs to update according to the category picked by the user

--- app/src/main/java/com/kiaan/collect_it/ui/progressbar/progressBarFragment.java
+++ app/src/main/java/com/kiaan/collect_it/ui/progressbar/progressBarFragment.java
@@ -4,18 +4,87 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kiaan.collect_it.R;
 import com.kiaan.collect_it.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.CURRENT_USER;
+
 public class progressBarFragment extends Fragment {
     private FragmentHomeBinding binding;
+    Spinner spnViewCatProgress;
+    Button btnViewCatProgress;
+    ProgressBar progress;
+    TextView tvProgress;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_progressbar, parent, false);
+
+        spnViewCatProgress = view.findViewById(R.id.spinnerSelectProgress);
+        btnViewCatProgress = view.findViewById(R.id.buttonViewCategoryProgress);
+        progress = view.findViewById(R.id.progressBarCategory);
+        tvProgress = view.findViewById(R.id.textViewCategoryPercent);
+
+        // populate the spinner box
+        // get database reference
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference categoryRef = rootRef.child("User").child(CURRENT_USER.displayName);
+
+        // set listener to retrieve data from realtime database
+        categoryRef.child("Category").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // declare temp arraylist
+                final List<String> list = new ArrayList<>();
+
+                // retrieve data from real time database
+                for (DataSnapshot addressSnapshot : dataSnapshot.getChildren()) {
+                    String value = addressSnapshot.getKey();
+                    if (value != null) {
+                        // add value to the list
+                        list.add(value);
+                    }
+                }
+                // declare and initialise Array Adapter to bind values to the spinner in real time
+                ArrayAdapter<String> addressAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, list);
+                addressAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spnViewCatProgress.setAdapter(addressAdapter);
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btnViewCatProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                progress.setProgress(20);
+                tvProgress.setText("20%");
+            }
+        });
 
         return view;
 
